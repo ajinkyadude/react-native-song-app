aa96a53c410a7861af2203d8dae34c38b265f9c9
ajinkyadude <ajinkyadude29@gmail.com>
2024-07-01 22:50:25 +0530

    1

---
+++ src/IgnoreTesting.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native"
+
+const GitIgnore = () => {
+    return(
+        <View> 
+            <Text>Hello</Text>
+        </View>
+    )
+}
+export default GitIgnore;
